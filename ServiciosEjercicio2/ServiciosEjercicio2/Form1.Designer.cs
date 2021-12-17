aeca65913dc814d31ee79d5f2594515ee8531607
Oscar fariña <61348449+ofaricamp@users.noreply.github.com>
2021-12-17 13:51:26 +0100

    Add files via upload

---
+++ ServiciosEjercicio2/ServiciosEjercicio2/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace ServiciosEjercicio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(292, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(367, 367);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(52, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(52, 59);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(234, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ruta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Palabra";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(15, 151);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 268);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 384);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ServiciosEjercicio2/ServiciosEjercicio2/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServiciosEjercicio2
+{
+    public partial class Form1 : Form
+    {
+        private readonly object l;
+        List<FileInfo> archivo = new List<FileInfo>();
+        string[] extensiones = { ".txt", ".doc", ".docx", ".docm",".odt",".pdf",".rtf",".csv",".xls",".xlsx",
+                                ".xlsm",".ods",".pps",".ppt",".ppsx",".pptx",".ppsm",".pptm",".potx",".odp"};
+        int contador = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = Image.FromFile("C:\\yamchaMuerto.jpg");
+        }
+
+        public void BuscaDirectorio(string contenidoTextBox)
+        {
+            DirectoryInfo info;
+            string directorio = Environment.ExpandEnvironmentVariables(contenidoTextBox);
+            //Directory.SetCurrentDirectory(directorio);
+            Directory.SetCurrentDirectory("C:\\DirectorioPadre\\Directorio2");
+            info = new DirectoryInfo(Directory.GetCurrentDirectory());
+            foreach (FileInfo archivos in info.GetFiles())
+            {
+                for (int i = 0; i < extensiones.Length; i++)
+                {
+                    if (archivos.Name.EndsWith(extensiones[i]))
+                    {
+                        archivo.Add(archivos);
+                        contador++;
+                    }
+                }
+            }
+            label3.Text = contador.ToString();
+        }
+
+        public void BuscadorDePalabras(string ruta, string palabra)
+        {
+
+            // FileInfo archivo = (FileInfo)file;
+            string contenedor = "";
+            double numeroDeVeces = 1;
+            //while (true)
+            //{
+            //lock (l)
+            //{
+            //  if (true)
+            // {
+            using (StreamReader reader = new StreamReader(ruta))
+            {
+                while ((contenedor = reader.ReadLine()) != null)
+                {
+                    label3.Text = numeroDeVeces.ToString();
+                   // numeroDeVeces++;
+                        System.Diagnostics.Debug.WriteLine(numeroDeVeces);
+                    if (contenedor.Contains(palabra))
+                    {
+                        numeroDeVeces++;
+
+                    }
+                }
+            }
+            // }
+            //}
+            //}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BuscadorDePalabras("C:\\DirectorioPadre\\Directorio2\\Txt3.txt", "Alvaro");
+        }
+    }
+}
