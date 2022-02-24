8521338a5108ca5a30b49a3964cbf539fab7f3da
Oscar fariña <61348449+ofaricamp@users.noreply.github.com>
2022-02-24 12:28:11 +0100

    Add files via upload

--- SERVBol3_Ejer3/SERVBol3_Ejer3/Cliente.cs
+++ SERVBol3_Ejer3/SERVBol3_Ejer3/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -10,14 +11,24 @@ namespace SERVBol3_Ejer3
 {
     class Cliente
     {
-        private string userName;
-        private IPEndPoint ip;
-        private Socket socket;
+      //  private string userName;
+      //  private IPEndPoint ip;
+      //  private Socket socket;
 
+        public Cliente(string name, Socket socket, IPEndPoint ip)
+        {
+            this.UserName = name;
+            this.Socket = socket;
+            this.Ip = ip;
+        }
         public string UserName { set; get; }
 
         public IPEndPoint Ip { set; get; }
 
         public Socket Socket { set; get; }
+
+        public StreamWriter Mensaje { set; get; }
+
+
     }
 }
--- SERVBol3_Ejer3/SERVBol3_Ejer3/Program.cs
+++ SERVBol3_Ejer3/SERVBol3_Ejer3/Program.cs
@@ -12,40 +12,47 @@ namespace SERVBol3_Ejer3
 {
     class Program
     {
+        //  static Cliente client = new Cliente();
         private static readonly object l = new object();
-        static List<StreamWriter> reido = new List<StreamWriter>();
+        static List<Cliente> clientecitos = new List<Cliente>();
 
-        static string UserName()
+        static void hiloCliente(object trueCliente)
         {
-
-            return "";
-        }
-        static void hiloCliente(object socket)
-        {
+            Cliente cliente;
             string mensaje;
+            string userName = "";
             bool apagar = false;
-            Socket cliente = (Socket)socket;
-       
+            Socket socket = (Socket)trueCliente;
 
-            IPEndPoint ieCliente = (IPEndPoint)cliente.RemoteEndPoint;
+            IPEndPoint ieCliente = (IPEndPoint)socket.RemoteEndPoint;
             Console.WriteLine("Connected with client {0} at port {1}", ieCliente.Address, ieCliente.Port);
 
-            using (NetworkStream ns = new NetworkStream(cliente))
+            using (NetworkStream ns = new NetworkStream(socket))
             using (StreamReader sr = new StreamReader(ns))
             using (StreamWriter sw = new StreamWriter(ns))
             {
+
                 string welcome = "Welcome to this great Thing";
                 sw.WriteLine(welcome);
                 sw.Flush();
-                reido.Add(sw);
+
+                sw.WriteLine("Inserte un nombre de usuario por fi :C");
+                sw.Flush();
+                userName = sr.ReadLine();
+
+                Console.WriteLine(userName + "    " + socket + "          " + ieCliente);
+                cliente = ClienteShokan(userName, socket, ieCliente);
+                clientecitos.Add(cliente);
+
+                cliente.Mensaje = sw;
 
                 while (!apagar)
                 {
                     try
                     {
+                        //mensaje = sr.ReadLine();
+                        //sw.WriteLine(mensaje);
                         mensaje = sr.ReadLine();
-                        sw.WriteLine(mensaje);
-                        sw.Flush();
 
                         if (mensaje != null)
                         {
@@ -54,23 +61,35 @@ namespace SERVBol3_Ejer3
                                 switch (mensaje)
                                 {
                                     case "#salir":
-                                        apagar = true;
+                                        Salir();
                                         break;
                                     case "#lista":
+                                        foreach (Cliente nombre in clientecitos)
+                                        {
+                                            Console.WriteLine(clientecitos.Count());
+                                            sw.WriteLine(nombre.UserName + " Conectado");
+                                            sw.Flush();
+                                        }
                                         break;
                                     default:
                                         Console.WriteLine("{0} dice: {1}", ieCliente.Address, mensaje);
-                                        foreach (StreamWriter mensajeDeUsuario in reido)
+                                        foreach (Cliente mesage in clientecitos)
                                         {
-                                            if (sw != mensajeDeUsuario)
-                                            {
-                                                mensajeDeUsuario.WriteLine("{0} dice: {1}",ieCliente.Address,mensaje);
-                                                mensajeDeUsuario.Flush();
-                                            }
+                                           
+                                           if (sw != mesage.Mensaje)
+                                           {
+                                                 mesage.Mensaje.WriteLine("{0}@{1} dice: {2}", cliente.UserName,
+                                                                          cliente.Ip, mensaje);
+                                                 mesage.Mensaje.Flush();
+                                           }
                                         }
                                         break;
                                 }
                             }
+                        }
+                        else
+                        {
+                            Salir();
                         }
                     }
                     catch (IOException)
@@ -80,10 +99,35 @@ namespace SERVBol3_Ejer3
                 }
                 Console.WriteLine("Finished Connection with {0}: {1}", ieCliente.Address, ieCliente.Port);
             }
-            cliente.Close();
+            void Salir()
+            {
+                //Console.WriteLine(clientecitos.Count());
+                for (int i = clientecitos.Count - 1; i >= 0; i--)
+                {
+                    if (ieCliente.Port == clientecitos[i].Ip.Port)
+                    {
+                        foreach (Cliente clientesale in clientecitos)
+                        {
+                            clientesale.Mensaje.WriteLine("{0}@{1} se desconecto",clientecitos[i].UserName,clientecitos[i].Ip.Address);
+                            clientesale.Mensaje.Flush();
+                        }
+                        clientecitos[i].Socket.Close();
+                        clientecitos[i].Mensaje.Close();
+                        clientecitos.RemoveAt(i);
+                        //Console.WriteLine(clientecitos.Count());
+                    }
+                }
+                apagar = true;
+            }
+            socket.Close();
+        }
+        static Cliente ClienteShokan(string name, Socket socket, IPEndPoint ipendPoint)
+        {
+            return new Cliente(name, socket, ipendPoint);
         }
         static void Main(string[] args)
         {
+            //   Cliente trueCliente = new Cliente();
             IPEndPoint ie = new IPEndPoint(IPAddress.Any, 10823);
             Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
@@ -92,6 +136,7 @@ namespace SERVBol3_Ejer3
             Console.WriteLine("Server waiting at port {0}", ie.Port);
             while (true)
             {
+                //   trueCliente.Socket = s.Accept();
                 Socket cliente = s.Accept();
                 Thread hilo = new Thread(hiloCliente);
                 hilo.Start(cliente);
